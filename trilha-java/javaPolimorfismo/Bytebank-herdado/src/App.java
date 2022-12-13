2ff3d3019943164c5e3c8b734f6b7b454024957b
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-12-13 18:08:49 +0000

    Aula 6 - Interfaces

--- trilha-java/javaPolimorfismo/Bytebank-herdado/src/App.java
+++ trilha-java/javaPolimorfismo/Bytebank-herdado/src/App.java
@@ -1,19 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Gerente g1 = new Gerente();
-        g1.setNome("Marco");
-        g1.setCpf("235568413");
-        g1.setSalario(5000.0);
-
-        System.out.println(g1.getNome());
-        System.out.println(g1.getCpf());
-        System.out.println(g1.getSalario());
-
+        Autenticavel g1 = new Gerente();
         g1.setSenha(2222);
-        boolean autenticou = g1.autentica(2222);
 
-        System.out.println(autenticou);
+        Autenticavel adm = new Administrador();
+        adm.setSenha(3);
 
-        System.out.println(g1.getBonificacao());
+        Autenticavel cliente = new Cliente();
+        cliente.setSenha(1);
+
+        SistemaInterno sistema = new SistemaInterno();
+
+        sistema.autentica(g1);
+        sistema.autentica(adm);
+        sistema.autentica(cliente);
     }
 }
