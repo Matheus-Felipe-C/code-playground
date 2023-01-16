110abd9a9b3c3627025a1bbd763500f7c007a711
Matheus Felipe <theusf40@hotmail.com>
2023-01-15 22:35:08 -0300

    curso java e java.lang Aula 3 - Distribuição do código

--- trilha-java/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ trilha-java/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,11 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe que representa uma Conta no Bytebank
+ * 
+ * @author matheus
+ * @version 0.1
+ */
 public abstract class Conta {
 
     protected double saldo;
@@ -8,6 +14,12 @@ public abstract class Conta {
     private Cliente titular;
     private static int total = 0;
     
+    /**
+     * Construtor para inicializar o objeto Conta a partir da agência e número
+     * 
+     * @param agencia
+     * @param numero
+     */
     public Conta(int agencia, int numero){
         Conta.total++;
         //System.out.println("O total de contas é " + Conta.total);
@@ -19,6 +31,12 @@ public abstract class Conta {
 
     public abstract void deposita(double valor);
 
+    /**
+     * Valor precisa ser menor ou igual ao saldo para poder realizar o saque
+     * 
+     * @param valor
+     * @throws SaldoInsuficienteException
+     */
     public void saca(double valor) throws SaldoInsuficienteException{
     	
         if(this.saldo < valor) {
