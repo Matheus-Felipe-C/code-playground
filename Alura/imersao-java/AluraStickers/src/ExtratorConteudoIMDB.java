26c2a5856337875793ab4d027a1cfd25f89b5d2a
Matheus Felipe <theusf40@hotmail.com>
2023-03-29 22:39:11 -0300

    refactor - separação de itens da App.java

---
+++ Alura/imersao-java/AluraStickers/src/ExtratorConteudoIMDB.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorConteudoIMDB implements ExtratorConteudo {
+    
+    public List<Conteudo> extrairConteudos(String json) {
+         // extrair só os dados que interessam (titulo, poster, classificação)
+         var parser = new JsonParser();
+         List<Map<String, String>> listaDeAtributos = parser.parse(json);
+ 
+         List<Conteudo> conteudos = new ArrayList<>();
+ 
+         //popular lista de conteudos
+         for (Map<String, String> atributos : listaDeAtributos) {
+             String titulo = atributos.get("title");
+             String urlImagem = atributos.get("image");
+             var conteudo = new Conteudo(titulo, urlImagem);
+ 
+             conteudos.add(conteudo);
+         }
+ 
+         return conteudos;
+    }
+}
